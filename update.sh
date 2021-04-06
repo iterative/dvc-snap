0ea24fa166e6fdec07d5d7587dc9db326c0fba95
Ruslan Kuprieiev <ruslan@iterative.ai>
2021-04-06 13:17:49 +0300

    update: fix typo

--- update.sh
+++ update.sh
@@ -12,4 +12,4 @@ PROJECT="iterative/dvc"
 GHAPI_URL="https://api.github.com/repos/$PROJECT/releases/latest"
 LATEST=$(curl --silent $GHAPI_URL | jq -r .tag_name)
 
-sed -i 's/^    source-tag: .*$/    source-tag: $LATEST/g' snap/snapcraft.yaml
+sed -i 's/^    source-tag: .*$/    source-tag: '"$LATEST"'/g' snap/snapcraft.yaml
