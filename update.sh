5632f2219ebf04626a43850436bf7d623ce7ef71
Ruslan Kuprieiev <kupruser@gmail.com>
2022-11-24 19:36:41 +0200

    snapcraft: use setuptools-scm
    Fixes #77

--- update.sh
+++ update.sh
@@ -13,5 +13,4 @@ GHAPI_URL="https://api.github.com/repos/$PROJECT/releases/latest"
 LATEST=$(curl --silent $GHAPI_URL | jq -r .tag_name)
 
 sed -ri -e 's/^(version:) .*$/\1 '"$LATEST"'/' \
-  -e 's/^(    source-tag:) .*$/\1 '"$LATEST"'/' \
-  -e 's/^(.*__version__ = )".*(".*)$/\1"'"$LATEST"'\2/' snap/snapcraft.yaml
+  -e 's/^(    source-tag:) .*$/\1 '"$LATEST"'/' snap/snapcraft.yaml
