2b118fd88351cafe835c84cd8b395e094db6e5dd
Ruslan Kuprieiev <kupruser@gmail.com>
2023-04-15 03:15:56 +0300

    update: remove unused code

--- update.sh
+++ update.sh
@@ -8,9 +8,5 @@ if [ ! $LATEST ]; then
   LATEST=$(./latest.sh)
 fi
 
-PROJECT="iterative/dvc"
-GHAPI_URL="https://api.github.com/repos/$PROJECT/releases/latest"
-LATEST=$(curl --silent $GHAPI_URL | jq -r .tag_name)
-
 sed -ri -e 's/^(version:) .*$/\1 '"$LATEST"'/' \
   -e 's/^(    source-tag:) .*$/\1 '"$LATEST"'/' snap/snapcraft.yaml
